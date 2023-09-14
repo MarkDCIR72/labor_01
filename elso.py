275f8c163234f5ea1b4253c84f0c236f200084dd
MarkDCIR72 <8hmark@gmail.com>
2023-09-14 10:32:11 +0200

    2. szinkron

--- elso.py
+++ elso.py
@@ -1,1 +1,10 @@
-print('üdv a fedélzeten')
+nev = input("Mi a neved:  ")
+print(f'Szia {nev}')
+print('üdv \na \nfedélzeten', end='')
+print('Szia', 'Józsi')
+print('Szia {0}'.format('Lala'))
+print('''Ez
+több sorba
+került''')
+print('Jobbra'.rjust(50,' '))
+print('Közép'.center(50,' '))
