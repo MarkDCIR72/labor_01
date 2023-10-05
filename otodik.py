503b3bf0c8b59a88321de2b2cd14c797f9fd402d
m <8hmark@gmail.com>
2023-10-05 10:31:54 +0200

    5 gyak

---
+++ otodik.py
@@ -0,0 +1,52 @@
+# Beléptető rendszer
+
+def regisztracio():
+    sikeres = True
+    felhasznalonev()
+    jelszo_bekerese()
+    i = 1
+    while not jelszo_ellenorzes():
+        print("Nem egyezik a két jelszó!")
+        i += 1
+        if i > 3:
+            sikeres = False
+            break
+    return sikeres
+
+def felhasznalonev():
+    felhasznalo_email = input("Kérek egy felhasználó nevet (email):")
+    while " " in felhasznalo_email or "@" not in felhasznalo_email or "." not in felhasznalo_email:
+        felhasznalo_email = input("Nem megfelelő az email!\nKérek egy felhasználó nevet (email):")
+
+def jelszo_bekerese():
+    felhasznalo_jelszava = input("Kérek egy jelszót (1,a,A, min 8 hosszú): ")
+    ok_jelszo = True
+    while ok_jelszo:
+        if len(felhasznalo_jelszava) < 8:
+            ok_jelszo = False
+        van = 0
+        for i in range(len(felhasznalo_jelszava)):
+            if felhasznalo_jelszava[i].isnumeric():
+                van +=1
+        if van == 0:
+            ok_jelszo = False
+        if ok_jelszo == False:
+            felhasznalo_jelszava = input("Nem megfelelő a jelszó!\nKérek egy jelszót (1,a,A, min 8 hosszú): ")
+
+
+def jelszo_ellenorzes():
+    pass
+    ok_jelszo = True
+    return ok_jelszo
+
+def beleptetes():
+    pass
+
+
+# Innen indul a program
+felhasznalo_email = ""
+felhasznalo_jelszava = ""
+if regisztracio():
+    beleptetes()
+else:
+    print("A sikertelem regisztráció miatt, nem történt beléptetés!")
